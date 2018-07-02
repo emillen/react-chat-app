7eed889be22c980729641cc9088e6e9deb3b67c2
Emil Lengman <emillen@kth.se>
2018-07-02 10:06:06 +0200

    change some styles

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -18,17 +18,17 @@ class LoginForm extends Component {
     if (this.props.isAuthenticated) return <Redirect to="/" />;
     else
       return (
-        <div className="card p-5">
+        <div className="card p-5 m-5">
           <h2 className="mb-4">Login</h2>
           <form onSubmit={this.submit}>
-            <div className="form-group row">
+            <div className="form-group row ">
               <label
                 htmlFor="inputPassword"
-                className="col-sm-2 col-form-label text-right"
+                className="col-lg-2 col-form-label text-lg-right text-sm-left"
               >
-                Email
+                Email:
               </label>
-              <div className="col-sm-10">
+              <div className="col-lg-10">
                 <input
                   type="text"
                   className="form-control"
@@ -38,14 +38,14 @@ class LoginForm extends Component {
                 />
               </div>
             </div>
-            <div className="form-group row">
+            <div className="form-group row ">
               <label
                 htmlFor="inputPassword"
-                className="col-sm-2 col-form-label text-right"
+                className="col-lg-2 col-form-label text-lg-right text-sm-left"
               >
-                Password
+                Password:
               </label>
-              <div className="col-sm-10">
+              <div className="col-lg-10 ">
                 <input
                   type="password"
                   className="form-control"
@@ -55,17 +55,13 @@ class LoginForm extends Component {
                 />
               </div>
             </div>
-            <div className="row">
-              <div className="col-sm-6">
-                <button
-                  onClick={this.submit}
-                  type="button"
-                  className="btn btn-dark"
-                >
-                  Login
-                </button>
-              </div>
-            </div>
+            <button
+              onClick={this.submit}
+              type="button"
+              className="btn btn-dark float-right"
+            >
+              Login
+            </button>
           </form>
 
           {this.props.error && (
