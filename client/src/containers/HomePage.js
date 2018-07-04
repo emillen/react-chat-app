d7b8dee42d05fd6820eb945529bd4ed14813d59f
Emil Lengman <emillen@kth.se>
2018-07-04 08:12:28 +0200

    remove unused 'me'-stuff

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,14 +1,10 @@
 import Home from "../components/Home";
-import { fetchMeFromServer } from "../operations/api";
 import { connect } from "react-redux";
 
 const mapStoreToProps = store => ({
-  isAuthenticated: store.auth.isAuthenticated,
-  data: store.me
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchData: () => fetchMeFromServer(dispatch)
 });
 
 export default connect(
