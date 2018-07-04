f045dc2ab5cacea33b1b4fab321ecc9b3bba0673
Emil Lengman <emillen@kth.se>
2018-07-04 08:09:46 +0200

    fake output

--- client/src/containers/RegisterPage.js
+++ client/src/containers/RegisterPage.js
@@ -1,10 +1,10 @@
 import RegisterForm from "../components/RegisterForm";
 import { connect } from "react-redux";
 
-const mapStoreToProps = store => ({
-});
+const mapStoreToProps = store => ({});
 
 const mapDispatchToProps = dispatch => ({
+	onSubmit: (email, username, password) => console.log(email, username, password),
 });
 
 export default connect(
