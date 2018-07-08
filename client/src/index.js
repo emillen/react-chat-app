2301ad57d02143b24e648a37d9de8de1fae0eee7
Emil Lengman <emillen@kth.se>
2018-07-08 09:49:49 +0200

    add bootstrap javascript

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js"
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -8,6 +9,7 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
+
 
 export const store = createStore(rootReducer);
 
